ce1295597dcbb9250e54cc45f4bee7a7ce6917b2
okumurakengo <okumurakengo1a@gmail.com>
2019-05-14 00:57:34 +0900

    20190513

---
+++ cs/29_access.cs
@@ -0,0 +1,33 @@
+using System;
+
+class User {
+    public string name;
+    public User(string name) {
+        this.name = name;
+    }
+    public virtual void SayHi() {
+        Console.WriteLine($"hi {name}");
+    }
+}
+
+class AdminUser: User {
+    public AdminUser(string name): base(name) {
+    }
+    public void SayHello() {
+        Console.WriteLine($"hello {name}");
+    }
+    public override void SayHi() {
+        Console.WriteLine($"[admin] hi {name}");
+    }
+}
+
+class MyApp {
+    static void Main() {
+        User tom = new User("Tom");
+        Console.WriteLine(tom.name);
+        tom.SayHi();
+        AdminUser bob = new AdminUser("Bob");
+        bob.SayHi();
+        bob.SayHello();
+    }
+}
