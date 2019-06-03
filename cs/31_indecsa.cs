9707f0142d65bffd6aabde890a48107b769d2e4b
okumurakengo <okumurakengo1a@gmail.com>
2019-06-04 00:06:53 +0900

    20190503

---
+++ cs/31_indecsa.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Team {
+    private string[] members = new string[3];
+    public string this[int i] {
+        get { return this.members[i]; }
+        set { this.members[i] = value; }
+    }
+}
+
+class MyApp {
+    static void Main() {
+        Team giants = new Team();
+        giants[0] = "taguchi";
+        giants[1] = "fkoji";
+        giants[2] = "dotinstall";
+        Console.WriteLine(giants[1]);
+    }
+}
