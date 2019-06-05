09d95d07d0c4cdad121b9b1fec7b8965c4926a78
okumurakengo <okumurakengo1a@gmail.com>
2019-06-06 00:19:51 +0900

    20190506

---
+++ cs/33_abstruct.cs
@@ -0,0 +1,26 @@
+using System;
+
+abstract class User {
+    public abstract void SayHi();
+}
+
+class Japanese: User {
+    public override void SayHi() {
+        Console.WriteLine("こんにちは");
+    }
+}
+
+class American: User {
+    public override void SayHi() {
+        Console.WriteLine("hi!");
+    }
+}
+
+class MyApp {
+    static void Main() {
+        Japanese aki = new Japanese();
+        aki.SayHi();
+        American tom = new American();
+        tom.SayHi();
+    }
+}
---
+++ js/ts/02_doc/13_never.ts
@@ -0,0 +1,5 @@
+function error(message: string): never {
+    throw new Error(message);
+}
+
+error("abc");
