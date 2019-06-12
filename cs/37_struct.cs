ed9523765ea64a9cad7db3a7dc10e273dee880fa
okumurakengo <okumurakengo1a@gmail.com>
2019-06-12 23:23:27 +0900

    20190612

---
+++ cs/37_struct.cs
@@ -0,0 +1,22 @@
+using System;
+
+struct Point {
+    public int X { get; }
+    public int Y { get; }
+    public Point(int x, int y) {
+        X = x;
+        Y = y;
+    }
+    public void GetInfo() {
+        Console.WriteLine($"({X}:{Y})");
+    }
+}
+
+class MyApp {
+    static void Main() {
+        Point p1 = new Point(5, 3);
+        Point p2 = new Point(12, 4);
+        p1.GetInfo();
+        p2.GetInfo();
+    }
+}
---
+++ unity/til.md
@@ -0,0 +1,2 @@
+20190612 ゲームを作っていこう
+https://dotinstall.com/lessons/basic_unity_v2/47307
