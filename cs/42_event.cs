80f9730caa9e80350edaf4afdcc835b609193ed4
okumurakengo <okumurakengo1a@gmail.com>
2019-06-20 23:31:38 +0900

    20190620

---
+++ cs/42_event.cs
@@ -0,0 +1,21 @@
+using System;
+
+delegate void MyEventHandler();
+
+class MyButton {
+    public event MyEventHandler MyEvent;
+    public void OnClicked() {
+        if (MyEvent != null) {
+            MyEvent();
+        }
+    }
+}
+
+class MyApp {
+    static void Main() {
+        MyButton btn = new MyButton();
+
+        btn.MyEvent += () => Console.WriteLine("Button Clicked!");
+        btn.OnClicked();
+    }
+}
