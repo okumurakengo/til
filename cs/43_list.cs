bc3b402ff1b638d4a04b79bf61bae8f85418210a
okumurakengo <okumurakengo1a@gmail.com>
2019-06-25 00:26:16 +0900

    20190624

---
+++ cs/43_list.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+class MyApp {
+    static void Main() {
+        // List<int> scores = new List<int>();
+        // scores.Add(30);
+        // scores.Add(80);
+        // scores.Add(60);
+
+        List<int> scores = new List<int>() {30, 80, 60};
+        scores[1] = 100;
+        Console.WriteLine(scores.Count);
+        foreach (var score in scores) {
+            Console.WriteLine(score);
+        }
+    }
+}
