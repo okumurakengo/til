77b0817fb0137e046f9ae38a92b6596b84f6732b
okumurakengo <okumurakengo1a@gmail.com>
2019-07-05 01:24:54 +0900

    20190704

--- cs/consoleApp/Program.cs
+++ cs/consoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace TeleprompterConsole
 {
@@ -13,7 +14,18 @@ namespace TeleprompterConsole
             {
                 while ((line = reader.ReadLine()) != null)
                 {
-                    yield return line;
+                    var words = line.Split(' ');
+                    var lineLength = 0;
+                    foreach (var word in words)
+                    {
+                        yield return word + " ";
+                        lineLength += word.Length + 1;
+                        if (lineLength > 700)
+                        {
+                            yield return Environment.NewLine;
+                            lineLength = 0;
+                        }
+                    }
                 }
             }
         }
@@ -24,6 +36,14 @@ namespace TeleprompterConsole
             foreach (var line in lines)
             {
                 Console.WriteLine(line);
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    var pause = Task.Delay(200);
+                    // Synchronously waiting on a task is an
+                    // anti-pattern. This will get fixed in later
+                    // steps.
+                    pause.Wait();
+                }
             }
         }
     }
