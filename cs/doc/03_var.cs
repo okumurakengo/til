4933c40d00b2afd939ec8c0aa3d81f84dfc15656
okumurakengo <okumurakengo1a@gmail.com>
2019-07-26 02:03:39 +0900

    20190725

---
+++ cs/doc/03_var.cs
@@ -0,0 +1,11 @@
+using System;
+public class HelloWorld
+{
+    static public void Main()
+    {
+        string firstFriend = "Maria";
+        string secondFriend = "Sage";
+        Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+        Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+    }
+}
