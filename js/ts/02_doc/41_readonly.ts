004d8437640da578ea657757a6db427ed89f75e1
okumurakengo <okumurakengo1a@gmail.com>
2019-08-06 01:02:16 +0900

    20190805

---
+++ js/ts/02_doc/41_readonly.ts
@@ -0,0 +1,9 @@
+class Octopus {
+    readonly name: string;
+    readonly numberOfLegs: number = 8;
+    constructor (theName: string) {
+        this.name = theName;
+    }
+}
+let dad = new Octopus("Man with the 8 strong legs");
+console.log(dad);
