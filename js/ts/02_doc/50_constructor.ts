2fdaa202a239fe7440ec653f716cf9a8dd822957
okumurakengo <okumurakengo1a@gmail.com>
2019-09-03 22:43:15 +0900

    20190903

---
+++ js/ts/02_doc/50_constructor.ts
@@ -0,0 +1,13 @@
+let Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+})();
+
+let greeter;
+greeter = new Greeter("world");
+console.log(greeter.greet());
