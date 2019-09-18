27c0b85298997a53e44a849c93aea3b7c17b075c
okumurakengo <okumurakengo1a@gmail.com>
2019-09-19 00:43:46 +0900

    20190919

---
+++ js/ts/02_doc/59_optional.ts
@@ -0,0 +1,12 @@
+function buildName(firstName: string, lastName?: string) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+}
+
+let result1 = buildName("Bob");                  // works correctly now
+// let result2 = buildName("Bob", "Adams", "Sr.");  // error, too many parameters
+let result3 = buildName("Bob", "Adams");         // ah, just right
+
+console.log(result1, result3)
