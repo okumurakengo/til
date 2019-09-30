0f2f1052268adb66578dcb7c157cce5a9a296e64
okumurakengo <okumurakengo1a@gmail.com>
2019-10-01 00:40:54 +0900

    20190930

---
+++ js/ts/02_doc/63_rest.ts
@@ -0,0 +1,6 @@
+function buildName(firstName: string, ...restOfName: string[]) {
+    return firstName + " " + restOfName.join(" ");
+}
+
+let buildNameFun: (fname: string, ...rest: string[]) => string = buildName;
+console.log(buildNameFun("a", "b", "c"))
