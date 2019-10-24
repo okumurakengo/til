9e26e5e342fb0a9db3f29574344ef29497b49fb0
okumurakengo <okumurakengo1a@gmail.com>
2019-10-25 01:34:25 +0900

    20191024

---
+++ js/ts/02_doc/76_generics.ts
@@ -0,0 +1,11 @@
+interface GenericIdentityFn {
+    <T>(arg: T): T;
+}
+
+function identity<T>(arg: T): T {
+    return arg;
+}
+
+let myIdentity: GenericIdentityFn = identity;
+
+console.log(myIdentity)
