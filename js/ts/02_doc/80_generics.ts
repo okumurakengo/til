ba9aaddc6915f9ce1a912d3090164f5476855516
okumurakengo <okumurakengo1a@gmail.com>
2019-11-01 00:58:39 +0900

    20191031

---
+++ js/ts/02_doc/80_generics.ts
@@ -0,0 +1,10 @@
+interface Lengthwise {
+    length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+    console.log(arg.length);  // Now we know it has a .length property, so no more error
+    return arg;
+}
+
+loggingIdentity({length: 10, value: 3});
