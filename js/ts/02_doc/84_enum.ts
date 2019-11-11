023b5f0b0a7ff8fabe7b43b80a575caea59cec26
okumurakengo <okumurakengo1a@gmail.com>
2019-11-11 22:32:22 +0900

    20191211

---
+++ js/ts/02_doc/84_enum.ts
@@ -0,0 +1,15 @@
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+console.log(Direction)
+console.log(Direction.Up)
+console.log(Direction.Down)
+console.log(Direction.Left)
+console.log(Direction.Right)
+console.log(Direction[0])
+console.log(Direction[1])
+console.log(Direction[2])
+console.log(Direction[3])
