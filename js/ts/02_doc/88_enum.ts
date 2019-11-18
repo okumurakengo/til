26fe6e59c1245fd017b197dc982694ca089d4cb1
okumurakengo <okumurakengo1a@gmail.com>
2019-11-18 22:42:48 +0900

    20191118

---
+++ js/ts/02_doc/88_enum.ts
@@ -0,0 +1,9 @@
+enum BooleanLikeHeterogeneousEnum {
+    No = 0,
+    Yes = "YES",
+}
+console.log(BooleanLikeHeterogeneousEnum)
+console.log(BooleanLikeHeterogeneousEnum.No)
+console.log(BooleanLikeHeterogeneousEnum.Yes)
+console.log(BooleanLikeHeterogeneousEnum[0])
+console.log(BooleanLikeHeterogeneousEnum['Yes'])
