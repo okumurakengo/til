27ada673f3ffd82aed21708b51e2f9345bacfcc8
okumurakengo <okumurakengo1a@gmail.com>
2020-01-23 23:20:56 +0900

    20200123

---
+++ js/ts/02_doc/121_union.ts
@@ -0,0 +1,11 @@
+function padLeft(value: string, padding: number | string) {
+    if (typeof padding === "number") {
+        return Array(padding + 1).join(" ") + value;
+    }
+    if (typeof padding === "string") {
+        return padding + value;
+    }
+    throw new Error(`Expected string or number, got '${padding}'.`);
+}
+
+console.log(padLeft("Hello world", 3)); // returns "    Hello world"
